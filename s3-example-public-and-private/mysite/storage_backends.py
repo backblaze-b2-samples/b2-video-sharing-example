77677956dc9da9f0143887944e39621d19aff41e
Pat Patterson <pat@backblaze.com>
2022-02-09 21:10:07 -0800

    Changes to make B2 work

--- s3-example-public-and-private/mysite/storage_backends.py
+++ s3-example-public-and-private/mysite/storage_backends.py
@@ -13,6 +13,7 @@ class PublicMediaStorage(S3Boto3Storage):
 
 class PrivateMediaStorage(S3Boto3Storage):
     location = settings.AWS_PRIVATE_MEDIA_LOCATION
+    bucket_name = settings.AWS_PRIVATE_BUCKET_NAME
     default_acl = 'private'
     file_overwrite = False
     custom_domain = False
